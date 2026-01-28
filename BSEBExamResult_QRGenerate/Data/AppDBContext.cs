cd94853a20afd8c30a575b588aeae3ee42bfaaf4
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-28 12:31:47 +0530

    Add AppDbContext

---
+++ BSEBExamResult_QRGenerate/Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BSEBExamResult_QRGenerate.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+    }
+}
