f0da84439c888f5835e898f9ddc1282e8bde6cc2
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-28 12:32:28 +0530

    Encrypt code

---
+++ BSEBExamResult_QRGenerate/Data/EncryptionHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BSEBExamResult_QRGenerate.Data
+{
+    public class EncryptionHelper
+    {
+        // ⚠ Keep this secret & same on QR reader side
+        private static readonly string EncryptionKey = "BSEB2026@SecureKey!";
+
+        public static string Encrypt(string plainText)
+        {
+            using var aes = Aes.Create();
+            aes.Key = GetKey();
+            aes.GenerateIV();
+
+            using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using var ms = new MemoryStream();
+
+            // prepend IV
+            ms.Write(aes.IV, 0, aes.IV.Length);
+
+            using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+            using (var sw = new StreamWriter(cs))
+            {
+                sw.Write(plainText);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            using var aes = Aes.Create();
+            aes.Key = GetKey();
+
+            var iv = new byte[16];
+            Array.Copy(fullCipher, 0, iv, 0, iv.Length);
+            aes.IV = iv;
+
+            using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using var ms = new MemoryStream(fullCipher, iv.Length, fullCipher.Length - iv.Length);
+            using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+            using var sr = new StreamReader(cs);
+
+            return sr.ReadToEnd();
+        }
+
+        private static byte[] GetKey()
+        {
+            using var sha = SHA256.Create();
+            return sha.ComputeHash(Encoding.UTF8.GetBytes(EncryptionKey));
+        }
+    }
+}
