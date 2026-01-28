64b1b9a3e06a9932ecf05f9706d47583fcd0ce7c
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-28 12:32:10 +0530

    Compression Code

---
+++ BSEBExamResult_QRGenerate/Data/CompressionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace BSEBExamResult_QRGenerate.Data
+{
+    public static class CompressionHelper
+    {
+        public static string Compress(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            using (var gs = new GZipStream(mso, CompressionMode.Compress))
+            {
+                msi.CopyTo(gs);
+            }
+            return Convert.ToBase64String(mso.ToArray());
+        }
+
+        public static string Decompress(string compressedText)
+        {
+            var bytes = Convert.FromBase64String(compressedText);
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            using var gs = new GZipStream(msi, CompressionMode.Decompress);
+            gs.CopyTo(mso);
+            return Encoding.UTF8.GetString(mso.ToArray());
+        }
+    }
+}
