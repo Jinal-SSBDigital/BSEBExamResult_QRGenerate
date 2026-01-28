b8283a91fc938d5184a399be74f3d6f9d84a792a
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-28 12:36:37 +0530

    Add Class for Subject

---
+++ BSEBExamResult_QRGenerate/Model/SubjectResult.cs
@@ -0,0 +1,16 @@
+﻿namespace BSEBExamResult_QRGenerate.Model
+{
+    public class SubjectResult
+    {
+        public string? Sub { get; set; }
+        public int? MaxMark { get; set; }
+        public int? PassMark { get; set; }
+        public string? Theory { get; set; }
+        public string? OB_PR { get; set; }
+        public string? GRC_THO { get; set; }
+        public string? GRC_PR { get; set; }
+        public string? TotSub { get; set; }
+        public string? SubjectGroupName { get; set; }
+        public string? CCEMarks { get; set; }
+    }
+}
