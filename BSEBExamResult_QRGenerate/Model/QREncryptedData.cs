06096188428f63d9a3f01c72dcd344ecdd06bd92
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-29 09:11:58 +0530

    changes for bulk data

---
+++ BSEBExamResult_QRGenerate/Model/QREncryptedData.cs
@@ -0,0 +1,12 @@
+﻿namespace BSEBExamResult_QRGenerate.Model
+{
+    public class QREncryptedData
+    {
+        public long Id { get; set; }
+        public string RollCode { get; set; }
+        public string RollNo { get; set; }
+        public string EncryptedData { get; set; }
+        public string QrPath { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
