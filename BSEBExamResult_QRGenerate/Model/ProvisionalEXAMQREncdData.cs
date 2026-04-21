44d082a4bb2d8d7434f0eb31d34983fd61619709
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-21 15:39:56 +0530

    Add ProvisionalEXAMQREncdData model
    Introduce ProvisionalEXAMQREncdData EF model mapped to the Provisional_EXAMQREncdData table. The class includes RollCode, RollNo, EncryptedData and QRLength properties to store encrypted exam QR payloads; a commented Key/Id line suggests adding a primary key if the table has one.

---
+++ BSEBExamResult_QRGenerate/Model/ProvisionalEXAMQREncdData.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BSEBExamResult_QRGenerate.Model
+{
+    [Table("Provisional_EXAMQREncdData")]
+    public class ProvisionalEXAMQREncdData
+    {
+       // [Key]
+       // public int Id { get; set; } // Add if table has PK (recommended)
+
+        public string RollCode { get; set; }
+
+        public string RollNo { get; set; }
+
+        public string EncryptedData { get; set; }
+
+        public int QRLength { get; set; }
+    }
+}
