00d193722e3251005cb0f1e98fe0397fb4724ad6
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-07-03 17:54:36 +0530

    Add compartment exam result models
    Add new model classes to support compartment exam results:
    
    - EXAM_QRComprtEncryptedData: For storing encrypted compartment exam QR data
    - CompartStudentResult: For compartment student result details
    
    Both classes follow the same structure as their regular exam counterparts, with additional ExamType field in CompartStudentResult.

--- BSEBExamResult_QRGenerate/Model/QREncryptedData.cs
+++ BSEBExamResult_QRGenerate/Model/QREncryptedData.cs
@@ -1,5 +1,6 @@
 ﻿namespace BSEBExamResult_QRGenerate.Model
 {
+    #region Jinal Nagar
     public class QREncryptedData
     {
         public long Id { get; set; }
@@ -11,4 +12,16 @@
         //public string QrPath { get; set; }
         public DateTime CreatedOn { get; set; }
     }
+    public class EXAM_QRComprtEncryptedData
+    {
+        public long Id { get; set; }
+        public string RollCode { get; set; }
+        public string RollNo { get; set; }
+        public string EncryptedData { get; set; }
+        public int Length { get; set; }
+
+        //public string QrPath { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+    #endregion
 }
--- BSEBExamResult_QRGenerate/Model/StudentResult.cs
+++ BSEBExamResult_QRGenerate/Model/StudentResult.cs
@@ -23,4 +23,26 @@ namespace BSEBExamResult_QRGenerate.Model
 
         public List<SubjectResult> SubjectResults { get; set; } = new();
     }
+    public class CompartStudentResult
+    {
+        public int? Status { get; set; }
+        public string? RollCode { get; set; }
+        public string? RollNo { get; set; }
+        public string? BsebUniqueID { get; set; }
+        public string? msg { get; set; }
+        public string? RegistrationNo { get; set; }
+        public DateTime? dob { get; set; }
+        public int? IsCCEMarks { get; set; }
+        public string? NameoftheCandidate { get; set; }
+        public string? FathersName { get; set; }
+        public string? CollegeName { get; set; }
+        public string? Faculty { get; set; }
+        public string? TotalAggregateMarkinNumber { get; set; }
+        public string? TotalAggregateMarkinWords { get; set; }
+        public string? Division { get; set; }
+        public string? ExamType { get; set; }
+        //public bool? IsCCEMarks { get; set; }
+
+        public List<SubjectResult> SubjectResults { get; set; } = new();
+    }
 }
